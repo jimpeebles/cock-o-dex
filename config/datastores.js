8eec1d1bdf6bfb9178b58e79b9d4381e4a342e34
jimpeebles <james@concepttoproduct.us>
2022-03-24 20:52:24 -0400

    base url, add db

--- config/datastores.js
+++ config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://cockodex:Tr00p3r.@cock-o-dex-db.chpntwqx4wtx.us-east-1.rds.amazonaws.com:3306/app',
 
   },
 
