0344bb295f2f4bbd0338966b133f3ca538af7c03
jimpeebles <james@concepttoproduct.us>
2022-03-25 19:12:39 -0400

    axios

--- assets/js/components/search.component.js
+++ assets/js/components/search.component.js
@@ -5,8 +5,10 @@
  *
  * @type {Component}
  * -----------------------------------------------------------------------------
- */
-
+ */ 
+ const TERM_SPIRIT = "spirit";
+ const TERM_MODIFIER = "modifier";
+ 
 parasails.registerComponent('search', {
   //  ╔═╗╦═╗╔═╗╔═╗╔═╗
   //  ╠═╝╠╦╝║ ║╠═╝╚═╗
@@ -17,13 +19,27 @@ parasails.registerComponent('search', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: function () {
-    return {};
+    return {
+      selectedTerms: [],
+      termOptions: [
+        { label: 'Gin', value: 'gin', type: TERM_SPIRIT },
+        { label: 'Vodka', value: 'vodka', type: TERM_SPIRIT },
+        { label: 'Whiskey', value: 'whiskey', type: TERM_SPIRIT },
+        { label: 'Rum', value: 'rum', type: TERM_SPIRIT },
+        { label: 'Lemon', value: 'lemon', type: TERM_MODIFIER },
+        { label: 'Lime', value: 'lime', type: TERM_MODIFIER },
+        { label: 'Passionfruit', value: 'passionfruit', type: TERM_MODIFIER },
+        { label: 'Ancho Reyes', value: 'ancho reyes', type: TERM_MODIFIER },
+        { label: 'Ancho Verde', value: 'ancho verde', type: TERM_MODIFIER },
+      ],
+      results: [],
+    };
   },
 
   //  ╦ ╦╔╦╗╔╦╗╦
   //  ╠═╣ ║ ║║║║
   //  ╩ ╩ ╩ ╩ ╩╩═╝
-  template: `<div>Welcome to your new component</div>`,
+  template: '#search-template',
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
@@ -37,5 +53,45 @@ parasails.registerComponent('search', {
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
-  methods: {},
+  methods: {
+    clear() {
+      this.selectedTerms = [];
+      this.results = [];
+    },
+    addTerm(term) {
+      this.selectedTerms.push(term);
+    },
+    async search() {
+      const shouldQuery = [];
+      this.selectedTerms.forEach((term) => {
+        shouldQuery.push({ term: { build: term.value } });
+      });
+      const fullQuery = {
+        query: {
+          bool: {
+            should: shouldQuery,
+          },
+        },
+      };
+      try {
+        const response = await $axios.request({
+          method: "post",
+          url: "https://3.83.108.116:9200/cocktail/_search",
+          data: fullQuery,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "ApiKey bGtrVnZuOEJ2ZnJPQ3VFQy0tZ2o6OTlZTWFUUzRRQVdaekhtU3FpWVRNZw==",
+          },
+          httpsAgent: httpsAgent,
+        });
+        console.log(response);
+        const results = (response.data?.hits?.hits ?? []).map((r) => r._source);
+        console.log(results);
+        this.results = results;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
 });
