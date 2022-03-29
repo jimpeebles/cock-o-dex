6ec25195ed4da9aacc5944b7210028a175330b54
jimpeebles <james@concepttoproduct.us>
2022-03-29 19:48:23 -0400

    fixx

--- api/controllers/search.js
+++ api/controllers/search.js
@@ -1,35 +1,34 @@
 const axios = require('axios').default;
 module.exports = async function search(req, res) {
-    const { terms } = req.query;
-    const termsArray = terms.split(',');
-    const shouldQuery = [];
-    termsArray.forEach((term) => {
-      shouldQuery.push({ term: { build: term.value } });
-    });
-    const fullQuery = {
-      query: {
-        bool: {
-          should: shouldQuery,
-        },
+  const { terms } = req.query;
+  const termsArray = terms.split(',');
+  const shouldQuery = [];
+  termsArray.forEach((term) => {
+    shouldQuery.push({ term: { build: term.value } });
+  });
+  const fullQuery = {
+    query: {
+      bool: {
+        should: shouldQuery,
       },
-    };
-    try {
-      const response = await axios.request({
-        method: 'post',
-        url: 'http://localhost:9200/cocktail/_search',
-        data: fullQuery,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      console.log(response);
-      const results = _.get(response.data, 'hits.hits', []).map(
+    },
+  };
+  try {
+    const response = await axios.request({
+      method: 'post',
+      url: 'http://localhost:9200/cocktail/_search',
+      data: fullQuery,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(response);
+    const results = _.get(response.data, 'hits.hits', []).map(
         (r) => r._source
-      );
-      console.log(results);
-      return res.json(results);
-    } catch (e) {
-      console.log(e);
-    }
-  },
+    );
+    console.log(results);
+    return res.json(results);
+  } catch (e) {
+    console.log(e);
+  }
 };
