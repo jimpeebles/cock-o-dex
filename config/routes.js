5cf1be6096b513ccef09fe602624a8631f762a4f
jimpeebles <james@concepttoproduct.us>
2022-03-29 19:43:24 -0400

    route

--- config/routes.js
+++ config/routes.js
@@ -16,7 +16,7 @@ module.exports.routes = {
   'GET /':                   { action: 'view-homepage-or-redirect' },
   'GET /welcome/:unused?':   { action: 'dashboard/view-welcome' },
 
-  'GET /search':                { action:   'search' },
+  'GET /search':                { action:   'elastic/search' },
 
   'GET /faq':                { action:   'view-faq' },
   'GET /legal/terms':        { action:   'legal/view-terms' },
